490466384d1002876b5d1d90e4a535e0d103e752
Maverick <GJS990825@GMAIL.COM>
2022-09-09 21:58:41 +0800

    Parallel input and output frame for comparing.

--- app.py
+++ app.py
@@ -1,69 +1,113 @@
-import io
-import os
 import sys
 import logging
+import random
 
 import cv2
-from PIL import Image
 from PyQt6 import QtGui
-from PyQt6.QtCore import QCoreApplication, Qt, QDir, QTimer, QSize
-from PyQt6.QtGui import QAction, QIcon, QPixmap, QImage, QKeySequence
+from PyQt6.QtCore import QCoreApplication, Qt, QTimerEvent
+from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QMainWindow, QMenu, QHBoxLayout, QStyle, \
-    QSlider, QFileDialog, QVBoxLayout, QLabel
+    QSlider, QFileDialog, QVBoxLayout, QLabel, QSizePolicy
 
 logging.basicConfig(level=logging.INFO)
 
 
+class InspectorImageProcessInterface:
+    def detect(self, image):
+        result = image.copy()
+        c = random.randint(0, 0xFFFFFF)
+        color = (c & 0xFF, c >> 8 & 0xFF, c >> 16 & 0xFF)
+        cv2.putText(result, 'PROCESSED', (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, color, 2)
+        return result
+
+
+image_process = InspectorImageProcessInterface()
+
+
+def clamp(n, min_n, max_n):
+    if n < min_n:
+        return min_n
+    elif n > max_n:
+        return max_n
+    else:
+        return n
+
+
 class ODInspector(QMainWindow):
-    video_file = 'videos/elephants_dream.mp4'
 
     def __init__(self):
         super().__init__()
 
         self.current_frame = None
-        self.window_size = QSize(700, 500)
+        self.current_frame_pixmap = None
+        self.current_output_frame = None
+        self.current_output_frame_pixmap = None
         self.camera = None
         self.video_name = None
         self.total_frame_number = None
-        self.frame_position = None
+        self.frame_position: int = 0
         self.video_timer_id = None
         self.frame_out_sync = False
 
+        # Some default settings
         self.skip_seconds = 15
         self.fps = 25
-        self.timer_interval = int(1000 / self.fps)
+        self.playback_speed = 1.0
+        self.timer_interval = lambda: int(1000 / (self.fps * self.playback_speed))
+        self.skip_frames = lambda: self.fps * self.skip_seconds
 
+        # Slider configuration
         self.frame_position_slider = QSlider(Qt.Orientation.Horizontal)
-        self.frame_position_slider.setRange(0, 1000)
-
         self.frame_position_slider.sliderReleased.connect(self.video_resume)
         self.frame_position_slider.sliderPressed.connect(self.video_pause)
         self.frame_position_slider.sliderMoved.connect(self.frame_position_slider_callback)
 
-        # Create a QHBoxLayout instance
-        central_widget = QWidget(self)
+        # Control center (just some buttons
+        control_center_layout = QHBoxLayout()
 
-        inner_layout = QHBoxLayout()
-        # Add widgets to the layout
-
-        self.button_pause_resume = QPushButton("Pause/Resume")
-        self.button_pause_resume.setShortcut(Qt.Key.Key_Space)
-
+        self.button_pause_resume = QPushButton("Resume")
+        play_pause_icon = self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay)
+        self.button_pause_resume.setIcon(play_pause_icon)
+        self.button_pause_resume.setShortcut('Ctrl+P')
         self.button_pause_resume.clicked.connect(self.video_pause_resume)
-        inner_layout.addWidget(self.button_pause_resume, 1)
-        self.button_stop = QPushButton("Stop")
+        control_center_layout.addWidget(self.button_pause_resume, 2)
+
+        self.button_stop = QPushButton("&Stop")
+        stop_icon = self.style().standardIcon(QStyle.StandardPixmap.SP_MediaStop)
+        self.button_stop.setIcon(stop_icon)
+        # self.button_pause_resume.setShortcut(QKeySequence('Ctrl+Z'))
         self.button_stop.clicked.connect(self.video_stop)
-        inner_layout.addWidget(self.button_stop, 1)
+        control_center_layout.addWidget(self.button_stop, 2)
 
-        outer_layout = QVBoxLayout()
-        self.frame_display = QLabel('Press Ctrl+O to open a video')
-        self.frame_display.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        outer_layout.addWidget(self.frame_display, 10)
-        outer_layout.addLayout(inner_layout, 1)
-        outer_layout.addWidget(self.frame_position_slider)
+        fastforward_icon = self.style().standardIcon(QStyle.StandardPixmap.SP_MediaSeekForward)
+        self.button_speed_double = QPushButton("2x")
+        self.button_speed_double.clicked.connect(self.speed_double)
+        self.button_speed_double.setIcon(fastforward_icon)
+        control_center_layout.addWidget(self.button_speed_double, 1)
 
-        # Set the layout on the application's window
-        central_widget.setLayout(outer_layout)
+        self.button_speed_half = QPushButton("0.5x")
+        self.button_speed_half.clicked.connect(self.speed_half)
+        self.button_speed_half.setIcon(fastforward_icon)
+        control_center_layout.addWidget(self.button_speed_half, 1)
+
+        # Input and output viewport
+        self.frame_input_display = QLabel('Press Ctrl+O to open a video')
+        self.frame_input_display.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.frame_input_display.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
+        self.frame_output_display = QLabel('Output will be here')
+        self.frame_output_display.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.frame_output_display.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
+        viewport_layout = QHBoxLayout()
+        viewport_layout.addWidget(self.frame_input_display, 1)
+        viewport_layout.addWidget(self.frame_output_display, 1)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(viewport_layout, 10)
+        main_layout.addLayout(control_center_layout, 1)
+        main_layout.addWidget(self.frame_position_slider)
+
+        central_widget = QWidget(self)
+        central_widget.setLayout(main_layout)
 
         # Configure menu bar items
         file_menu = QMenu('&File', self)
@@ -91,12 +135,15 @@ class ODInspector(QMainWindow):
         self.setWindowTitle("OD Inspector")
         self.setWindowIcon(QIcon('images/icons/ic_app.png'))
         self.statusBar().showMessage('Ready')
-        self.setGeometry(300, 300, 700, 500)
+        self.resize(800, 500)
         self.center()
         self.show()
 
-    def timerEvent(self, timer_event) -> None:
-        # frame = self.get_a_frame(self.frame_counter)
+    def timerEvent(self, event: QTimerEvent):
+        if event.timerId() == self.video_timer_id:
+            self.video_timer_handler()
+
+    def video_timer_handler(self):
         if not self.frame_out_sync:
             self.frame_position += 1
         else:
@@ -107,38 +154,54 @@ class ODInspector(QMainWindow):
         success, img = self.camera.read()
         if success:
             self.current_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            q_image = QtGui.QImage(self.current_frame.data,
+                                   self.current_frame.shape[1],
+                                   self.current_frame.shape[0],
+                                   QtGui.QImage.Format.Format_RGB888)
+            self.current_frame_pixmap = QtGui.QPixmap.fromImage(q_image)
+
+            self.current_output_frame = image_process.detect(self.current_frame)
+            q_image = QtGui.QImage(self.current_output_frame.data,
+                                   self.current_output_frame.shape[1],
+                                   self.current_output_frame.shape[0],
+                                   QtGui.QImage.Format.Format_RGB888)
+            self.current_output_frame_pixmap = QtGui.QPixmap.fromImage(q_image)
+
             self.display_current_frame()
 
     def display_current_frame(self):
-        q_image = QtGui.QImage(self.current_frame.data,
-                               self.current_frame.shape[1],
-                               self.current_frame.shape[0],
-                               QtGui.QImage.Format.Format_RGB888)
-        q_pixmap = QtGui.QPixmap.fromImage(q_image)
-        q_pixmap_scaled = q_pixmap.scaled(self.frame_display.size(),
-                                          Qt.AspectRatioMode.KeepAspectRatio,
-                                          Qt.TransformationMode.FastTransformation)
-        self.frame_display.setPixmap(q_pixmap_scaled)
+        scaled = self.current_frame_pixmap.scaled(self.frame_input_display.size(),
+                                                  Qt.AspectRatioMode.KeepAspectRatio,
+                                                  Qt.TransformationMode.SmoothTransformation)
+        self.frame_input_display.setPixmap(scaled)
+
+        scaled = self.current_output_frame_pixmap.scaled(self.frame_output_display.size(),
+                                                         Qt.AspectRatioMode.KeepAspectRatio,
+                                                         Qt.TransformationMode.SmoothTransformation)
+        self.frame_output_display.setPixmap(scaled)
+
         self.frame_position_slider.setValue(self.frame_position)
         logging.debug(f'Showing {self.frame_position}th frame')
 
     def frame_position_slider_callback(self):
         position = self.frame_position_slider.value()
         self.update_frame_position(position)
+        logging.debug(f'Slider moved to {position}')
 
     def update_frame_position(self, position):
         self.frame_position = position
         self.frame_out_sync = True
 
-    def keyReleaseEvent(self, a0: QtGui.QKeyEvent) -> None:
+    def keyReleaseEvent(self, a0: QtGui.QKeyEvent):
         if self.video_timer_id is None:
             a0.accept()
             return
-
         if a0.key() == Qt.Key.Key_Right:
-            self.update_frame_position(self.frame_position + self.skip_seconds * self.fps)
+            position = clamp(self.frame_position + self.skip_frames(), 0, self.total_frame_number)
+            self.update_frame_position(position)
         elif a0.key() == Qt.Key.Key_Left:
-            self.update_frame_position(self.frame_position - self.skip_seconds * self.fps)
+            position = clamp(self.frame_position - self.skip_frames(), 0, self.total_frame_number)
+            self.update_frame_position(position)
         else:
             a0.accept()
 
@@ -146,12 +209,22 @@ class ODInspector(QMainWindow):
         if self.video_timer_id is not None:
             self.killTimer(self.video_timer_id)
             self.video_timer_id = None
+            play_pause_icon = self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay)
+            self.button_pause_resume.setIcon(play_pause_icon)
+            self.button_pause_resume.setText('Resume')
             logging.info('Video playback paused')
 
     def video_resume(self):
         if self.video_timer_id is None:
-            self.video_timer_id = self.startTimer(self.timer_interval, Qt.TimerType.CoarseTimer)
+            play_pause_icon = self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPause)
+            self.button_pause_resume.setIcon(play_pause_icon)
+            self.button_pause_resume.setText('Pause')
+            self.video_timer_id = self.startTimer(self.timer_interval(), Qt.TimerType.CoarseTimer)
             logging.info('Video playback resumed')
+
+    def video_restart(self):
+        self.video_pause()
+        self.video_resume()
 
     def video_stop(self):
         self.video_pause()
@@ -163,7 +236,8 @@ class ODInspector(QMainWindow):
         self.frame_position = None
         self.video_timer_id = None
         self.frame_out_sync = False
-        self.frame_display.setText('Video unloaded')
+        self.frame_input_display.setText('Video unloaded')
+        self.frame_output_display.setText('Video unloaded')
         self.widgets_enabled(False)
         logging.info('Video playback stopped')
 
@@ -171,6 +245,8 @@ class ODInspector(QMainWindow):
         self.button_pause_resume.setEnabled(status)
         self.button_stop.setEnabled(status)
         self.frame_position_slider.setEnabled(status)
+        self.button_speed_half.setEnabled(status)
+        self.button_speed_double.setEnabled(status)
         logging.info(f'Widgets: {status}')
 
     def video_pause_resume(self):
@@ -181,6 +257,17 @@ class ODInspector(QMainWindow):
             self.video_pause()
         else:
             self.video_resume()
+
+    def set_playback_speed(self, speed):
+        self.playback_speed = speed
+        self.video_restart()
+        self.statusBar().showMessage(f'Playback speed: {speed}x', 1000)
+
+    def speed_double(self):
+        self.set_playback_speed(self.playback_speed * 2)
+
+    def speed_half(self):
+        self.set_playback_speed(self.playback_speed * 0.5)
 
     def open_file(self):
         file_name, video_type = QFileDialog.getOpenFileName(self,
@@ -195,16 +282,18 @@ class ODInspector(QMainWindow):
     def load_video(self):
         self.camera = cv2.VideoCapture(self.video_name)
         self.total_frame_number = int(self.camera.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.video_timer_id = self.startTimer(self.timer_interval, Qt.TimerType.CoarseTimer)
+        self.fps = self.camera.get(cv2.CAP_PROP_FPS)
+        if self.fps > 60:
+            logging.error(f'Abnormal fps: {self.fps}, reset to default fps')
+            self.fps = 25
+        else:
+            logging.info(f'Video fps: {self.fps}')
         self.frame_position = 0
         self.frame_position_slider.setRange(0, self.total_frame_number)
         self.widgets_enabled(True)
+        self.video_resume()
 
-    def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
-        self.window_size = a0.size()
-        a0.accept()
-
-    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+    def closeEvent(self, event: QtGui.QCloseEvent):
         reply = QMessageBox.question(self, 'Message',
                                      "Are you sure to quit?",
                                      QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
