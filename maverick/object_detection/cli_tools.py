294121d2c50a69363d0c4442132368bf5920caa9
Maverick <GJS990825@GMAIL.COM>
2022-11-05 20:35:00 +0800

    Command line tools api v2 support.

--- maverick/object_detection/cli_tools.py
+++ maverick/object_detection/cli_tools.py
@@ -1,9 +1,9 @@
 import argparse
 import os
 
-from maverick.object_detection import ImageProcessingHelper, ODServiceInterface
 from maverick.object_detection.analyzer import *
-from maverick.object_detection.utils import camel_to_snake
+from maverick.object_detection.api.v2 import ODServiceInterface, DetectionConfig
+from maverick.object_detection.api.v2.helper import ImageProcessingHelper
 
 analyzer_class_names = [cls.__name__ for cls in ODResultAnalyzer.__subclasses__()]
 
@@ -11,16 +11,13 @@ analyzer_class_names = [cls.__name__ for cls in ODResultAnalyzer.__subclasses__(
 def launch(service: ODServiceInterface):
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, help='source video')
-    parser.add_argument('--model_name', type=str, default='yolov5m')
-
-    for name in analyzer_class_names:
-        parser.add_argument(f'--{camel_to_snake(name)}', type=str, default=None, help=f'{name} configuration file')
+    parser.add_argument(f'--config_name', type=str, default=None, help=f'configuration name')
     parser.add_argument('--output_directory', type=str, default='./')
     parser.add_argument('--output_video', type=str, default='output.mp4', help='video result output')
     opt = parser.parse_args()
 
+    config_name = opt.config_name
     video_path = opt.input
-    model_name = opt.model_name
     output_directory = opt.output_directory
     output_video = opt.output_video
 
@@ -37,23 +34,22 @@ def launch(service: ODServiceInterface):
     recorder = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), int(video_fps), size)
 
     analyzers: list[ODResultAnalyzer] = []
-
-    for name in analyzer_class_names:
-        config_file = getattr(opt, camel_to_snake(name), None)
-        if config_file is None:
-            continue
-        analyzers.extend(globals()[name].from_file(config_file))
+    configs = DetectionConfig.from_file('configs/detection_configs.json')
+    config = next(config for config in configs if config.name == config_name)
+    for analyzer_path in config.analyzer_files:
+        analyzers.extend(ODResultAnalyzer.from_file(analyzer_path))
 
     if len(analyzers) == 0:
         print('No analyzers configured')
 
     processing_helper = ImageProcessingHelper(service, analyzers)
+    processing_helper.set_analyzers(analyzers)
     processing_helper.service.update_models()
-    processing_helper.set_current_model(model_name)
+    processing_helper.update_colors()
 
     for analyzer in analyzers:
         analyzer.redirect_saving_path(output_directory)
-        analyzer.set_class_name_converter(service.current_model.class_name_converter)
+        analyzer.set_class_name_converter(service.convert_name)
     print(analyzers)
 
     output_playback_fps = 0
@@ -65,7 +61,7 @@ def launch(service: ODServiceInterface):
         if not success:
             break
         input_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        output_rgb = processing_helper.detect(input_rgb)  # TODO: use threading here?
+        output_rgb = processing_helper.detect(input_rgb, config.model_names)  # TODO: use threading here?
         recorder.write(cv2.cvtColor(output_rgb, cv2.COLOR_RGB2BGR))
 
         now = time.time()
